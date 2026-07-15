0b5e60dd641a21509b6809b21695ecb449c11e0a
Olalekan Ogundare <ogogundare@gmail.com>
2026-07-15 13:27:14 -0400

    Updating Apps Code

--- infra/terraform/modules/aws-load-balancer-controller/outputs.tf
+++ infra/terraform/modules/aws-load-balancer-controller/outputs.tf
@@ -3,7 +3,15 @@ output "iam_role_arn" {
   value       = aws_iam_role.this.arn
 }
 
+# output "helm_release_name" {
+#   description = "Helm release name for the AWS Load Balancer Controller."
+#   value       = helm_release.this.name
+# }
+
 output "helm_release_name" {
-  description = "Helm release name for the AWS Load Balancer Controller."
-  value       = helm_release.this.name
+  value = try(helm_release.this[0].name, null)
+}
+
+output "service_account_name" {
+  value = try(kubernetes_service_account.this[0].metadata[0].name, null)
 }
